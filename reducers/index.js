aeb981b82fe4be4f5aca1c93f6d1863d978fa4f6
justin-ig <kon0147@naver.com>
2023-10-19 06:05:46 +0900

    test

--- reducers/index.js
+++ reducers/index.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 import userSlice from './user';
 import postSlice from './post';
 
-axios.defaults.baseURL = 'http://localhost:8000';
-//axios.defaults.baseURL = 'https://node-ingeun.koyeb.app';
+//axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://node-ingeun.koyeb.app';
 axios.defaults.withCredentials = true;
 // (이전상태, 액션) => 다음상태
 const rootReducer = combineReducers({
